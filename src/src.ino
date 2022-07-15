6a6a5aa072d3cba54f363b629739e3fcdd5a090b
tunapro1234 <tunagul54@gmail.com>
2022-07-15 16:57:51 +0300

    yavaştan hardwarele denemeye başlamak lazım

--- src/src.ino
+++ src/src.ino
@@ -29,13 +29,21 @@ bool calib_test(calibration_data_t* _data, int first_sensor) {
 		read_min[i] = read_normal[i];
 	}
 
+	int counter = 0;
 	int start_time = millis();
 	while (millis() - start_time <= 6000) {
-		serialf("Passed time: %d\n", millis() - start_time);
-		delay(1000);
+		for (int i=0; i < _data->header.sensor_num; i++) {
+			int read_value = analogRead(first_sensor+i);
+			if (read_value > read_max[i])		read_max[i] = read_value;
+			else if (read_value < read_min[i])	read_min[i] = read_value;
+		} counter++;
+	} 
+	blink(100, 30);
+	serialf("Read %d sensors %d times in 6 seconds.\n", _data->header.sensor_num, counter);
+	for (int i=0; i < _data->header.sensor_num; i++) {
+		serialf("Sensor %d:\n\tmin: %d\n\tmax: %d\n", i, read_min[i], read_max[i]);
 	}
 
-	blink(100, 30);
 	free(read_normal);
 	free(read_max);
 	free(read_min);
@@ -53,27 +61,19 @@ void setup() {
 	for (int i=0; i < 3; i++)
 		_calibration_data_g->sensor_datas[i] = {i, i+1, i+2};
 
-	calibration_write(0, _calibration_data_g);
-
-#ifdef _DEBUG
-	Serial.println("Data written to eeprom.");
-#endif
+	//calibration_write(0, _calibration_data_g);
+	//Serial.println("Data written to eeprom.");
 }
 
 void loop() {
-#ifdef _DEBUG
 	Serial.println("\nLoop begin");
-#endif
-
-	calibration_data_t *read_data = calibration_read(0);
+	//calibration_data_t *read_data = calibration_read(0);
 	calib_test(_calibration_data_g, _first_sensor_pin_g);
 
 
 
-#ifdef _DEBUG
 	Serial.println("End.");
-#endif
-	free(read_data);
+	//free(read_data);
 	delay(9000);
 }
 
