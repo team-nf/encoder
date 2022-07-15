da995c3fab2087df88cdb07452330096d75ef1b1
tunapro1234 <tunagul54@gmail.com>
2022-07-15 16:46:34 +0300

    blink macro and calib_test

--- src/src.ino
+++ src/src.ino
@@ -7,12 +7,47 @@
 
 calibration_data_t *_calibration_data_g;
 
+#define blink(time, repetition) ({ \
+	for (int i=0; i < repetition+2; i++) { \
+		digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN)); \
+		delay(time); \
+	} \
+})
+
+bool calib_test(calibration_data_t* _data, int first_sensor) {
+#ifdef _DEBUG
+	if (!calibration_check_exists(&_data->header, &_example_meta_g)) { return false; }
+#endif
+	int *read_max = (int *)malloc(_data->header.sensor_num * sizeof(int));
+	int *read_min = (int *)malloc(_data->header.sensor_num * sizeof(int));
+	int *read_normal = (int *)malloc(_data->header.sensor_num * sizeof(int));
+	blink(100, 30);
+
+	for (int i=0; i < _data->header.sensor_num; i++) {
+		read_normal[i] = analogRead(first_sensor+i);
+		read_max[i] = read_normal[i];
+		read_min[i] = read_normal[i];
+	}
+
+	int start_time = millis();
+	while (millis() - start_time <= 6000) {
+		serialf("Passed time: %d\n", millis() - start_time);
+		delay(1000);
+	}
+
+	blink(100, 30);
+	free(read_normal);
+	free(read_max);
+	free(read_min);
+}
+
 void setup() {
 #ifdef _DEBUG
 	Serial.begin(115200);
 	Serial.println("\nStarted.");
 #endif
-
+	
+	pinMode(LED_BUILTIN, OUTPUT);
 	_calibration_data_g = (calibration_data_t *)malloc(_sensor_num_g * sizeof(calibration_data_t));
 	_calibration_data_g->header = _example_meta_g;
 	for (int i=0; i < 3; i++)
@@ -21,9 +56,6 @@ void setup() {
 	calibration_write(0, _calibration_data_g);
 
 #ifdef _DEBUG
-	Serial.println("Configuration data: ");
-	calibration_print(_calibration_data_g);
-
 	Serial.println("Data written to eeprom.");
 #endif
 }
@@ -32,20 +64,16 @@ void loop() {
 #ifdef _DEBUG
 	Serial.println("\nLoop begin");
 #endif
-	calibration_data_t *read_data = calibration_read(0);
-#ifdef _DEBUG
-	calibration_print(read_data);
-	Serial.print("Check Exists: ");
-	Serial.println(calibration_check_exists(&read_data->header, &_calibration_data_g->header));
-	Serial.print("Check Sensor Data: ");
-	Serial.println(calibration_check_sensor_data(read_data->sensor_datas, _calibration_data_g->header.sensor_num));
 
-	Serial.print("Check: ");
-	Serial.println(calibration_check(read_data, &_calibration_data_g->header));
+	calibration_data_t *read_data = calibration_read(0);
+	calib_test(_calibration_data_g, _first_sensor_pin_g);
+
+
+
+#ifdef _DEBUG
 	Serial.println("End.");
 #endif
-
 	free(read_data);
-	delay(12000);
+	delay(9000);
 }
 
