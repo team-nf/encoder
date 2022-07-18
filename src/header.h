4ef3596947bd0f48307e123ccf9d6f075bd36c40
tunapro1234 <tunagul54@gmail.com>
2022-07-18 11:05:11 +0300

    fix header files

--- src/header.h
+++ src/header.h
@@ -1,24 +1,6 @@
 #ifndef _HEADER_H_INCLUDED
 #define _HEADER_H_INCLUDED
 
-/* release için derleme hızı artsın diye */
-#ifdef _DEBUG
-#define serialf(...) ({ \
-	int _size = snprintf(NULL, 0, __VA_ARGS__) + 1; \
-	char* _str = (char *)malloc(_size * sizeof(char)); \
-	snprintf(_str, _size, __VA_ARGS__); \
-	Serial.print(_str); \
-	free(_str); \
-})
-#endif
-
-#define blink(time, repetition) ({ \
-	for (int i=0; i < repetition+2; i++) { \
-		digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN)); \
-		delay(time); \
-	} \
-})
-
 /* typedef enum bool { false=0, true=1 }; */
 
 enum encoder_mode_g {
@@ -33,11 +15,32 @@ typedef struct sensor_data_t {
 	int _min, _normal, _max;
 } sensor_data_t;
 
+#include "config.h"
+
+/* release için derleme hızı artsın diye */
+#ifdef _DEBUG
+#define serialf(...) ({ \
+	int _size = snprintf(NULL, 0, __VA_ARGS__) + 1; \
+	char* _str = (char *)malloc(_size * sizeof(char)); \
+	snprintf(_str, _size, __VA_ARGS__); \
+	Serial.print(_str); \
+	free(_str); \
+})
 #endif
+
+
+#define blink(time, repetition) ({ \
+	for (int i=0; i < repetition+2; i++) { \
+		digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN)); \
+		delay(time); \
+	} \
+})
+
+#endif
+
 
 /* config file include */
-
-#ifndef _CONFIG_H_INCLUDED
-#define _CONFIG_H_INCLUDED
-#include "config.h"
-#endif
+/* #ifndef _CONFIG_H_INCLUDED */
+/* #define _CONFIG_H_INCLUDED */
+/* #include "config.h" */
+/* #endif */
